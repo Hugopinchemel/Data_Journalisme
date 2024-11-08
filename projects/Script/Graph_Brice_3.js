f108cc982b20492a03a4285365776c2f7fea9ff9
Hugo Pinchemel <148193347+Hugopinchemel@users.noreply.github.com>
2024-11-08 14:04:47 +0100

    better graph

--- projects/Script/Graph_Brice_3.js
+++ projects/Script/Graph_Brice_3.js
@@ -1,27 +1,27 @@
-google.charts.load('current', {'packages': ['corechart']});
+google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
 
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
-        ['Year', 'Utilisation du 2FA (%) ', 'Piratages échoués (%) '],
-        ['2016', 20, 60],
-        ['2017', 30, 65],
-        ['2018', 40, 70],
-        ['2019', 50, 80],
-        ['2020', 56, 90],
-        ['2021', 60, 92],
-        ['2022', 62, 94],
-        ['2023', 64, 95],
-        ['2024', 69, 98],
+        ['Year', 'Utilisation du 2FA (%) ', 'Piratages réussis (%) '],
+        ['2016',  20, 40],
+        ['2017',  30, 35],
+        ['2018',  40, 30],
+        ['2019',  50, 20],
+        ['2020',  56, 10],
+        ['2021',  60, 8],
+        ['2022',  62, 6],
+        ['2023',  64, 5],
+        ['2024',  69, 2],
     ]);
 
 
     var options = {
-        title: 'Utilisation du 2FA et taux de piratages échoués chez les particuliers (2016-2024)',
-        echo: 'Google Threat Horizons',
+        title: 'Utilisation du 2FA et taux de piratages réussis chez les particuliers (2016-2024)',
+        echo : 'Google Threat Horizons' ,
         curveType: 'function',
-        legend: {position: 'bottom'}
+        legend: { position: 'bottom' }
     };
 
 
